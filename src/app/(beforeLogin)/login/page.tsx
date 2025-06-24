488f3782f232efe37164bd587f67a90594784287
Witsel-Blue <witselblue@gmail.com>
2025-06-24 12:43:16 +0900

    home

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import {useRouter} from "next/navigation";
+import Home from "@/app/(beforeLogin)/_component/Home";
 
 export default function Login() {
     const router = useRouter();
     router.replace('/i/flow/login');
-    return null;
+    return (
+        <Home />
+    );
 }
-
 // router.push : 뒤로가기 실행 시 바로 전 히스토리로
 // router.replace : 뒤로가기 시행 시 이전 페이지로
--- src/app/(beforeLogin)/signup/page.tsx
+++ src/app/(beforeLogin)/signup/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import {useRouter} from "next/navigation";
+import Home from "@/app/(beforeLogin)/_component/Home";
 
 export default function Signup() {
     const router = useRouter();
     router.replace('/i/flow/signup');
-    return null;
+    return (
+        <Home />
+    );
 }
