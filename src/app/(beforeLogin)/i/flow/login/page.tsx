d4dc9adb51a5ce29bb15a4b74e67097d645abd22
Witsel-Blue <witselblue@gmail.com>
2025-06-24 11:08:38 +0900

    home login page

---
+++ src/app/(beforeLogin)/i/flow/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LogIn() {
+    return (
+        <main>
+            로그인 모달
+        </main>
+    )
+}
---
+++ src/app/(beforeLogin)/layout.tsx
@@ -0,0 +1,14 @@
+import {ReactNode} from 'react';
+import styles from '@/app/page.module.css';
+
+type Props = {children: ReactNode, modal: ReactNode};
+
+export default function Layout({children, modal}: Props ) { // 패러랠 라우트
+    return (
+        <div className={styles.container}>
+            비포 로그인 레이아웃
+            {children}
+            {modal}
+        </div>
+    )
+}
