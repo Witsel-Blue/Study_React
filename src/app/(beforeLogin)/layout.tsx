259ee08979b123611d35c39b5d2d41df6ef332bb
Witsel-Blue <witselblue@gmail.com>
2025-06-25 10:50:11 +0900

    home layout

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -6,7 +6,6 @@ type Props = {children: ReactNode, modal: ReactNode};
 export default function Layout({children, modal}: Props ) { // 패러랠 라우트
     return (
         <div className={styles.container}>
-            비포 로그인 레이아웃
             {children}
             {modal}
         </div>
