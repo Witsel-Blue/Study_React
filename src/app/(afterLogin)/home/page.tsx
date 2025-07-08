8255e0a0f7bd4cdd42497759547eceaa5f4021f0
Witsel-Blue <witselblue@gmail.com>
2025-07-08 14:36:12 +0900

    home > tab

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,7 +1,26 @@
+import style from './home.module.css';
+import TabProvider from './_component/TabProvider';
+import Tab from "@/app/(afterLogin)/home/_component/Tab";
+import PostForm from '@/app/(afterLogin)/home/_component/PostForm';
+import Post from "@/app/(afterLogin)/_component/Post";
+
 export default function Home() {
     return (
-        <main>
-            홈페이지
+        <main className={style.main}>
+            <TabProvider>
+                <Tab />
+                <PostForm />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+            </TabProvider>
         </main>
     )
 }
