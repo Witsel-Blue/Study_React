b096a8ba3217edb866e0cdeb1cb9ce0784152db2
Witsel-Blue <witselblue@gmail.com>
2025-07-18 11:44:13 +0900

    msw 프론트 세팅

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,7 +1,5 @@
 import { setupWorker } from 'msw/browser'
-import { handlers } from './handlers'
-
-// This configures a Service Worker with the given request handlers.
+import { handlers } from './handlers';
 const worker = setupWorker(...handlers)
 
 export default worker;
