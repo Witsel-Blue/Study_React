8d9fda1e14eb864cb4c3be7568bbe05e6323dcdc
Witsel-Blue <witselblue@gmail.com>
2025-07-21 15:33:13 +0900

    [BACK] Next15 Form 컴포넌트로 변경

--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -3,11 +3,17 @@ import BackButton from '@/app/(afterLogin)/_component/BackButton';
 import SearchForm from '@/app/(afterLogin)/_component/SearchForm';
 import Tab from '@/app/(afterLogin)/search/_component/Tab';
 import Post from '@/app/(afterLogin)/_component/Post';
+import { headers } from 'next/headers';
 
-type Props = {
-    searchParams: { q: string, f?: string, pf?: string };
-}
-export default function Search({ searchParams }: Props) {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+
+export default async function Search() {
+    const headersList = await headers();
+
+    const rawUrl = headersList.get('x-url') || BASE_URL;
+    const url = new URL(rawUrl);
+    const q = url.searchParams.get('q') || '';
+
     return (
         <main className={style.main}>
             <div className={style.searchTop}>
@@ -16,25 +22,16 @@ export default function Search({ searchParams }: Props) {
                         <BackButton />
                     </div>
                     <div className={style.formZone}>
-                        <SearchForm q={searchParams.q} pageName='search' />
+                        <SearchForm q={q} pageName="search" />
                     </div>
                 </div>
                 <Tab />
             </div>
             <div className={style.list}>
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                {/*<SearchResult searchParams={searchParams} />*/}
+                {Array.from({ length: 10 }).map((_, i) => (
+                    <Post key={i} />
+                ))}
             </div>
         </main>
-    )
+    );
 }
