c93cf6c6966b55ca4bf3e8266ad354bc13a55446
Witsel-Blue <witselblue@gmail.com>
2025-07-28 12:56:56 +0900

    [BACK] user page

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -17,6 +17,7 @@ type Props = {
 }
 export default function Post({ noImage, post }: Props) {
     const target = post;
+    if (!post?.User) return null;
 
     return (
         <PostArticle post={target}>
