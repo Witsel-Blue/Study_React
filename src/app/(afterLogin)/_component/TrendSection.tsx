d104ab8ff007fb0497f6a164acd1100968fb5bf9
Witsel-Blue <witselblue@gmail.com>
2025-07-30 11:26:06 +0900

    [BACK] trend

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -1,30 +1,31 @@
 'use client';
 
 import style from './trendSection.module.css';
-import Trend from './Trend';
+import Trend from '@/app/(afterLogin)/_component/Trend';
 import { usePathname } from 'next/navigation';
 import { useSession } from 'next-auth/react';
+import { useQuery } from '@tanstack/react-query';
+import { getTrends } from '@/app/(afterLogin)/_lib/getTrends';
+import { Hashtag } from '@/model/Hashtag';
 
 export default function TrendSection() {
+    const { data: session } = useSession();
+    const { data } = useQuery<Hashtag[]>({
+        queryKey: ['trends'],
+        queryFn: getTrends,
+        staleTime: 60 * 1000,
+        gcTime: 300 * 1000,
+        enabled: !!session?.user // 로그인 시에만 데이터 가져옴
+    })
+
     const pathname = usePathname();
     if (pathname === '/explore') return null;
-
-    const { data } = useSession();
-    if (data?.user) {
+    if (session?.user) {
         return (
             <div className={style.trendBg}>
                 <div className={style.trend}>
                     <h3>나를 위한 트렌드</h3>
-                    <Trend />
-                    <Trend />
-                    <Trend />
-                    <Trend />
-                    <Trend />
-                    <Trend />
-                    <Trend />
-                    <Trend />
-                    <Trend />
-                    <Trend />
+                    {data?.map((trend) => <Trend trend={trend} key={trend.tagId} />)}
                 </div>
             </div>
         );
